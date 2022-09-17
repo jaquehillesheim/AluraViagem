ca588ef871a6aa070fdcf77d8790c8cc512e842d
Jaqueline Hillesheim <jaquehillesheim@hotmail.com>
2022-09-17 14:03:39 -0300

    primeiro commit

--- AluraViagem/HomeTableViewHeader.swift
+++ AluraViagem/HomeTableViewHeader.swift
@@ -8,13 +8,16 @@
 import UIKit
 
 class HomeTableViewHeader: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    // MARK: IBOutlets
+    
+    
+    @IBOutlet weak var tituloLabel: UILabel!
+    @IBOutlet weak var headeView: UIView!
+    @IBOutlet weak var bannerImageView: UIImageView!
+    
+    func configuraView() {
+        headeView.backgroundColor = UIColor(red: 30.0/255.0, green: 59.0/255.0, blue: 119.0/255, alpha: 1)
     }
-    */
-
+    
 }
